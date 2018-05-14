0785167ec0f9302376a8f58c6b1c13aeaa8aeda6
Robert Tabares <robtabares92@gmail.com>
2018-05-14 17:52:18 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+def instantiate_new_array
+  arr = [];
+  return arr
+end
+
+
+def array_with_two_elements
+  arr = [1,2]
+  return arr
+end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
