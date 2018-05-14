de0df336aa0a857177fb32ab9aeb9428e442af1d
Robert Tabares <robtabares92@gmail.com>
2018-05-14 17:50:42 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,7 +10,17 @@ def array_with_two_elements
 end
 
 def first_element(array)
-
   return array.first
+end
 
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
+
+def first_element_with_array_methods(array)
+  return array.first
 end
